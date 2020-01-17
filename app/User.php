08ce6afec6ab4b14e03be331a6808b52ed0d4e8c
Zahid Anwar <zahidanwar976@gmail.com>
2020-01-16 19:43:55 -0800

    User file update

--- app/User.php
+++ app/User.php
@@ -7,7 +7,6 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    //
     protected $table = "users";
     protected $primaryKey = "id";
      protected $fillable = ['name', 'email', 'password'];
